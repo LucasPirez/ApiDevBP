60e1c69645fbedae4e0d942976c12d879af3554e
Lucas Pirez <lucaspirez42@gmail.com>
2024-01-18 13:31:36 -0300

    add Exepctions

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -25,56 +25,99 @@ namespace ApiDevBP.Controllers
         [HttpPost]
         public async Task<IActionResult> SaveUser(UserModel user)
         {
+            try
+            {
+
             bool result = _userServices.SaveUser(user);
             return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in GetOne method for user with ID {user}");
+                return StatusCode(500, "Internal Server Error");
+            }
         }
 
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
+            try
+            {
+
             IEnumerable<UserModel>? users = _userServices.GetUsers();
 
             if (users == null) return NotFound();
 
 
             return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in GetUsers method");
+                return StatusCode(500, "Internal Server Error");
+            }
 
         }
 
         [HttpGet("GetOne")]
         public async Task<IActionResult> GetOne(int id)
         {
+            try
+            {
+
             var user = _userServices.GetUser(id);
 
             if (user == null) return NotFound();
 
 
             return Ok(user);
-
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in GetOne method with userId:{id} ");
+                return StatusCode(500, "Internal Server Error");
+            }
         }
 
         [HttpPut]
         public async Task<IActionResult> EditUser(int id, UserModel user)
         {
+            try
+            {
+
             var userEdited = _userServices.EditUser(user, id);
 
             if (!userEdited) return NotFound();
 
 
             return Ok(userEdited);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in EditUser method with user: {user} ");
+                return StatusCode(500, "Internal Server Error");
+            }
 
         }
 
         [HttpDelete]
         public async Task<IActionResult> DeleteUser(int id)
         {
+            try
+            {
+
             var userEdited = _userServices.DeleteUser(id);
 
             if (!userEdited) return NotFound();
 
 
             return Ok(userEdited);
-
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error in DeleteUser method with userId: {id}");
+                return StatusCode(500, "Internal Server Error");
+            }
         }
 
     }
